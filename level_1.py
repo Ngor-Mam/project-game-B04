ca86493107758b5286c508872dc187d8aae09d63
Phanit Chea <phanit.chea@student.passerellesnumeriques.org>
2023-10-26 20:55:56 +0700

    #5 add button back

--- level_1.py
+++ level_1.py
@@ -22,3 +22,17 @@ backgroung_level1 = Image.open('images/level-1-images/background_level1.png')
 backgroung_level1_size = backgroung_level1.resize((window_width, window_height))
 show_backgrund_level1 = ImageTk.PhotoImage(backgroung_level1_size)
 canvas.create_image(0, 0, image=show_backgrund_level1,anchor="nw" )
+
+button_back = Image.open('images/level_3-images/button_back.png')
+button_back_size = button_back.resize((70,70))
+btn = ImageTk.PhotoImage(button_back_size)
+canvas.create_image(50,50,image=btn)
+
+def button_back():
+    print("Button clicked!")
+    os.system("python levels_game.py")
+    window.destroy()
+      # Replace "animetion.py" with the correct file path if needed
+
+button = tk.Button(frame, image=btn, command=button_back,bg="black")
+button.place(x=10, y=10)
